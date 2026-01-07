03e93f62de33031802b47827b4aa26530087ab9f
JeraldJF <jeraldj@sanketika.in>
2026-01-07 14:56:46 +0530

    #SB-I403: fix: support nodebb core apis

--- src/services/discussion/implementation/discussion-service-impl.ts
+++ src/services/discussion/implementation/discussion-service-impl.ts
@@ -38,7 +38,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     createPost(data: any, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/topics`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/topics`)
             .withBearerToken(true)
             .withUserToken(true)
             .withBody(data)
@@ -91,7 +91,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     votePost(pid: number, data, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/posts/${pid}/vote`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/posts/${pid}/vote`)
             .withBearerToken(true)
             .withUserToken(true)
             .withBody(data)
@@ -105,7 +105,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     deleteVotePost(pid: number, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.DELETE)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/posts/${pid}/vote`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/posts/${pid}/vote`)
             .withBearerToken(true)
             .withUserToken(true)
             .build();
@@ -118,7 +118,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     bookmarkPost(pid: number, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/posts/${pid}/bookmark`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/posts/${pid}/bookmark`)
             .withBearerToken(true)
             .withUserToken(true)
             .build();
@@ -131,7 +131,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     deleteBookmarkPost(pid: number, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.DELETE)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/posts/${pid}/bookmark`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/posts/${pid}/bookmark`)
             .withBearerToken(true)
             .withUserToken(true)
             .build();
@@ -144,7 +144,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     replyPost(tid: number, data: any, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/topics/${tid}`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/topics/${tid}`)
             .withBearerToken(true)
             .withUserToken(true)
             .withBody(data)
@@ -385,7 +385,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     editPost(pid: number, data, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/posts/${pid}`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/posts/${pid}`)
             .withBearerToken(true)
             .withUserToken(true)
             .withBody(data)
@@ -398,7 +398,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     deletePost(pid: number, uid: number, config?) {
         const apiRequest: CsRequest = new CsRequest.Builder()
             .withType(CsHttpRequestType.DELETE)
-            .withPath(`${config ? config.apiPath : this.apiPath}/v2/posts/${pid}?uid=${uid}`)
+            .withPath(`${config ? config.apiPath : this.apiPath}/posts/${pid}?uid=${uid}`)
             .withBearerToken(true)
             .withUserToken(true)
             .build();
@@ -460,7 +460,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     deleteTopic(tid: number, config?: CsDiscussionServiceConfig): Observable<any> {
         const apiRequest: CsRequest = new CsRequest.Builder()
         .withType(CsHttpRequestType.DELETE)
-        .withPath(`${config ? config.apiPath : this.apiPath}/v2/topics/${tid}`)
+        .withPath(`${config ? config.apiPath : this.apiPath}/topics/${tid}`)
         .withBearerToken(true)
         .withUserToken(true)
         .build();
@@ -473,7 +473,7 @@ export class DiscussionServiceImpl implements CsDiscussionService {
     editTopic(tid: number, data: any, config?: CsDiscussionServiceConfig): Observable<CsAttachForumResponse> {
         const apiRequest: CsRequest = new CsRequest.Builder()
         .withType(CsHttpRequestType.POST)
-        .withPath(`${config ? config.apiPath : this.apiPath}/v2/topics/${tid}`)
+        .withPath(`${config ? config.apiPath : this.apiPath}/topics/${tid}`)
         .withBearerToken(true)
         .withUserToken(true)
         .withBody(data)
